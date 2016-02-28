ff056b59c7de29f906f08e7f5350c505a7390558
Carl Youngblood <carl@youngbloods.org>
2016-02-27 17:30:40 -0700

    Add all files in legacy site to gh-pages

---
+++ wpscripts/global_navtree.js
@@ -0,0 +1,28 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+'bIsWebPath':true,
+'sUrl':'index.html',
+'sTarget':'_self'
+},
+{   'sTitle':'About Us',
+'bIsWebPath':true,
+'sUrl':'about.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Gallery',
+'bIsWebPath':true,
+'sUrl':'gallery.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Events',
+'bIsWebPath':true,
+'sUrl':'news.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Contact Us',
+'bIsWebPath':true,
+'sUrl':'contact.html',
+'sTarget':'_self'
+}]
+});
